b058f3639a718dd128b0c355683d89e6db56c76e
fimbul <fimbul77@gmail.com>
2015-02-03 21:07:18 +0900

    Refactoring tuple_cat.

--- shiro/tuple/tuple_cat.hpp
+++ shiro/tuple/tuple_cat.hpp
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 //
 // Copyright (c) fimbul 2014.
 // https://github.com/fimbul/Shiro
@@ -25,171 +25,144 @@ namespace shiro {
 namespace detail {
 namespace tuple {
 
-template <typename TupleTypes1, typename TupleTypes2>
-struct tuple_cat_return_impl4;
-template <typename... TupleTypes1, typename... TupleTypes2>
-struct tuple_cat_return_impl4<shiro::tuple<TupleTypes1...>,
-                              shiro::tuple<TupleTypes2...>> {
-  using type = shiro::tuple<TupleTypes1..., TupleTypes2...>;
+template <typename Tuple1, typename Tuple2>
+struct tuple_cat_return_impl2;
+template <typename... Types1, typename... Types2>
+struct tuple_cat_return_impl2<shiro::tuple<Types1...>, shiro::tuple<Types2...>> {
+  using type = shiro::tuple<Types1..., Types2...>;
 };
 
-template <typename TupleTypes, typename LIndices, typename RIndices>
-struct tuple_cat_return_impl3;
-template <typename T>
-struct tuple_cat_return_impl3<shiro::tuple<T>, std::index_sequence<>,
-                              std::index_sequence<0>> {
-  using type = T;
-};
-template <>
-struct tuple_cat_return_impl3<shiro::tuple<>, std::index_sequence<>,
-                              std::index_sequence<>> {
+template <typename TypeTuple, typename Indices1, typename Indices2>
+struct tuple_cat_return_impl;
+
+template <typename TypeTuple, std::size_t L>
+struct tuple_cat_return_impl<TypeTuple,
+                             shiro::index_sequence_range<L, L>,
+                             shiro::index_sequence_range<L, L>> {
   using type = shiro::tuple<>;
 };
-template <typename TupleTypes, std::size_t... LIndices, std::size_t... RIndices>
-struct tuple_cat_return_impl3<TupleTypes, std::index_sequence<LIndices...>,
-                              std::index_sequence<RIndices...>> {
-  using type = typename tuple_cat_return_impl4<
-      typename tuple_cat_return_impl3<
-          shiro::tuple<shiro::tuple_element_t<LIndices, TupleTypes>...>,
-          shiro::make_index_sequence_range<0, sizeof...(LIndices) / 2>,
-          shiro::make_index_sequence_range<sizeof...(LIndices) / 2,
-                                           sizeof...(LIndices)>>::type,
-      typename tuple_cat_return_impl3<
-          shiro::tuple<shiro::tuple_element_t<RIndices, TupleTypes>...>,
-          shiro::make_index_sequence_range<0, sizeof...(RIndices) / 2>,
-          shiro::make_index_sequence_range<sizeof...(RIndices) / 2,
-                                           sizeof...(RIndices)>>::type>::type;
+
+template <typename TypeTuple, std::size_t L>
+struct tuple_cat_return_impl<TypeTuple,
+                             shiro::index_sequence_range<L, L>,
+                             shiro::index_sequence_range<L, L + 1>> {
+  using type = shiro::tuple_element_t<L, TypeTuple>;
 };
 
-template <typename Tuple, typename Indices>
-struct tuple_cat_return_impl2;
-template <typename Tuple, std::size_t... Indices>
-struct tuple_cat_return_impl2<Tuple, std::index_sequence<Indices...>> {
-  using type = shiro::tuple<shiro::tuple_element_t<Indices, Tuple>...>;
-};
-
-template <typename... Tuples>
-struct tuple_cat_return_impl {
-  using type = typename tuple_cat_return_impl3<
-      shiro::tuple<Tuples...>,
-      shiro::make_index_sequence_range<0, sizeof...(Tuples) / 2>,
-      shiro::make_index_sequence_range<sizeof...(Tuples) / 2,
-                                       sizeof...(Tuples)>>::type;
+template <typename TypeTuple, std::size_t L, std::size_t M, std::size_t N>
+struct tuple_cat_return_impl<TypeTuple,
+                             shiro::index_sequence_range<L, M>,
+                             shiro::index_sequence_range<M, N>> {
+  using type = typename tuple_cat_return_impl2<
+      typename tuple_cat_return_impl<
+        TypeTuple,
+        shiro::index_sequence_range<L, L + (M - L) / 2>,
+        shiro::index_sequence_range<L + (M - L) / 2, M>
+      >::type,
+      typename tuple_cat_return_impl<
+        TypeTuple,
+        shiro::index_sequence_range<M, M + (N - M) / 2>,
+        shiro::index_sequence_range<M + (N - M) / 2, N>
+      >::type
+    >::type;
 };
 
 template <typename... Tuples>
 struct tuple_cat_return {
-  using type = typename tuple_cat_return_impl<typename tuple_cat_return_impl2<
-      std::remove_reference_t<Tuples>,
-      shiro::make_index_sequence<shiro::tuple_size<
-          std::remove_reference_t<Tuples>>::value>>::type...>::type;
+  using type = typename tuple_cat_return_impl<
+      shiro::tuple<std::decay_t<Tuples>...>,
+      shiro::index_sequence_range<0, sizeof...(Tuples) / 2>,
+      shiro::index_sequence_range<sizeof...(Tuples) / 2, sizeof...(Tuples)>
+    >::type;
 };
 
-template <typename Indices1, typename Indices2>
-struct tuple_cat_indices_impl3;
+template <typename IndexTuple1, typename IndexTuple2>
+struct make_tuple_cat_index_impl2;
 template <std::size_t... Indices1, std::size_t... Indices2>
-struct tuple_cat_indices_impl3<std::index_sequence<Indices1...>,
-                               std::index_sequence<Indices2...>> {
+struct make_tuple_cat_index_impl2<std::index_sequence<Indices1...>,
+                                  std::index_sequence<Indices2...>> {
   using type = std::index_sequence<Indices1..., Indices2...>;
 };
 
-template <typename TupleIndices, typename LIndices, typename RIndices>
-struct tuple_cat_indices_impl2;
-template <typename T>
-struct tuple_cat_indices_impl2<shiro::tuple<T>, std::index_sequence<>,
-                               std::index_sequence<0>> {
-  using type = T;
-};
-template <typename T>
-struct tuple_cat_indices_impl2<shiro::tuple<T>, std::index_sequence<>,
-                               std::index_sequence<>> {
+template <typename IndexTuples, typename Indices1, typename Indices2>
+struct make_tuple_cat_index_impl;
+
+template <typename IndexTuples, std::size_t L>
+struct make_tuple_cat_index_impl<IndexTuples,
+                           shiro::index_sequence_range<L, L>,
+                           shiro::index_sequence_range<L, L>> {
   using type = std::index_sequence<>;
 };
-template <typename TupleIndices, std::size_t... LIndices,
-          std::size_t... RIndices>
-struct tuple_cat_indices_impl2<TupleIndices, std::index_sequence<LIndices...>,
-                               std::index_sequence<RIndices...>> {
-  using type = typename tuple_cat_indices_impl3<
-      typename tuple_cat_indices_impl2<
-          shiro::tuple<shiro::tuple_element_t<LIndices, TupleIndices>...>,
-          shiro::make_index_sequence_range<0, sizeof...(LIndices) / 2>,
-          shiro::make_index_sequence_range<sizeof...(LIndices) / 2,
-                                           sizeof...(LIndices)>>::type,
-      typename tuple_cat_indices_impl2<
-          shiro::tuple<shiro::tuple_element_t<RIndices, TupleIndices>...>,
-          shiro::make_index_sequence_range<0, sizeof...(RIndices) / 2>,
-          shiro::make_index_sequence_range<sizeof...(RIndices) / 2,
-                                           sizeof...(RIndices)>>::type>::type;
+
+template <typename IndexTuples, std::size_t L>
+struct make_tuple_cat_index_impl<IndexTuples,
+                           shiro::index_sequence_range<L, L>,
+                           shiro::index_sequence_range<L, L + 1>> {
+  using type = shiro::tuple_element_t<L, IndexTuples>;
 };
 
-template <typename... TupleIndices>
-struct tuple_cat_indices_impl {
-  using type = typename tuple_cat_indices_impl2<
-      shiro::tuple<TupleIndices...>,
-      shiro::make_index_sequence_range<0, sizeof...(TupleIndices) / 2>,
-      shiro::make_index_sequence_range<sizeof...(TupleIndices) / 2,
-                                       sizeof...(TupleIndices)>>::type;
+template <typename IndexTuples, std::size_t L, std::size_t M, std::size_t N>
+struct make_tuple_cat_index_impl<IndexTuples,
+                                 shiro::index_sequence_range<L, M>,
+                                 shiro::index_sequence_range<M, N>> {
+  using type = typename make_tuple_cat_index_impl2<
+      typename make_tuple_cat_index_impl<
+        IndexTuples,
+        shiro::index_sequence_range<L, L + (M - L) / 2>,
+        shiro::index_sequence_range<L + (M - L) / 2, M>
+      >::type,
+      typename make_tuple_cat_index_impl<
+        IndexTuples,
+        shiro::index_sequence_range<M, M + (N - M) / 2>,
+        shiro::index_sequence_range<M + (N - M) / 2, N>
+      >::type
+    >::type;
 };
 
 template <typename... Tuples>
-struct tuple_cat_indices {
-  using type = typename tuple_cat_indices_impl<shiro::make_index_sequence<
-      shiro::tuple_size<std::remove_reference_t<Tuples>>::value>...>::type;
+struct make_tuple_cat_index1 {
+  using type = typename make_tuple_cat_index_impl<
+      shiro::tuple<shiro::make_index_sequence<
+        shiro::tuple_size<std::decay_t<Tuples>>::value>...>,
+      shiro::index_sequence_range<0, sizeof...(Tuples) / 2>,
+      shiro::index_sequence_range<sizeof...(Tuples) / 2, sizeof...(Tuples)>
+    >::type;
 };
 
-template <std::size_t N, typename Indices>
-struct tuple_cat_indices2_impl2;
+template <std::size_t N, typename IndexTuple>
+struct make_tuple_cat_index2_impl2;
+
 template <std::size_t N, std::size_t... Indices>
-struct tuple_cat_indices2_impl2<N, std::index_sequence<Indices...>> {
-  using type = std::index_sequence<(static_cast<void>(Indices), N)...>;
+struct make_tuple_cat_index2_impl2<N, std::index_sequence<Indices...>> {
+  using type = std::index_sequence<(Indices, N)...>;
 };
 
-template <typename Indices, typename... TupleIndices>
-struct tuple_cat_indices2_impl;
-template <std::size_t... Indices, typename... TupleIndices>
-struct tuple_cat_indices2_impl<std::index_sequence<Indices...>,
-                               TupleIndices...> {
-  using type = typename tuple_cat_indices_impl<
-      typename tuple_cat_indices2_impl2<Indices, TupleIndices>::type...>::type;
+template <typename Indices, typename... IndexTuples>
+struct make_tuple_cat_index2_impl;
+
+template <std::size_t... Indices, typename... IndexTuples>
+struct make_tuple_cat_index2_impl<std::index_sequence<Indices...>, IndexTuples...> {
+  using type = shiro::tuple<typename make_tuple_cat_index2_impl2<Indices, IndexTuples>::type...>;
 };
 
 template <typename... Tuples>
-struct tuple_cat_indices2 {
-  using type = typename tuple_cat_indices2_impl<
-      shiro::make_index_sequence<sizeof...(Tuples)>,
-      shiro::make_index_sequence<
-          shiro::tuple_size<std::remove_reference_t<Tuples>>::value>...>::type;
+struct make_tuple_cat_index2 {
+  using type = typename make_tuple_cat_index_impl<
+      typename make_tuple_cat_index2_impl<
+        shiro::make_index_sequence<sizeof...(Tuples)>,
+        shiro::make_index_sequence<
+          shiro::tuple_size<std::decay_t<Tuples>>::value>...
+        >::type,
+      shiro::index_sequence_range<0, sizeof...(Tuples) / 2>,
+      shiro::index_sequence_range<sizeof...(Tuples) / 2, sizeof...(Tuples)>
+    >::type;
 };
 
-template <bool>
-struct tuple_cat_forward {
-  template <std::size_t I1, std::size_t I2, typename T, typename Tuples>
-  static constexpr const T& eval(Tuples&& t) {
-    return shiro::get<I1>(shiro::get<I2>(t));
-  }
-};
-template <>
-struct tuple_cat_forward<false> {
-  template <std::size_t I1, std::size_t I2, typename T, typename Tuples>
-  static constexpr T&& eval(Tuples&& t) {
-    return std::forward<T>(shiro::get<I1>(shiro::get<I2>(t)));
-  }
-};
-
-template <typename RTypes, typename Tuples, std::size_t... Indices1,
-          std::size_t... Indices2>
-constexpr RTypes tuple_cat(Tuples&& ts, const std::index_sequence<Indices1...>&,
-                           const std::index_sequence<Indices2...>&) {
-  return RTypes(
-      shiro::detail::tuple::tuple_cat_forward<
-          std::is_const<std::remove_reference_t<
-              shiro::tuple_element_t<Indices2, Tuples>>>::value>::
-          template eval<
-              Indices1, Indices2,
-              shiro::tuple_element_t<
-                  Indices1, std::remove_reference_t<shiro::tuple_element_t<
-                                Indices2, std::remove_reference_t<Tuples>>>>>(
-              ts)...);
+template <typename ReturnType, typename Tuples, std::size_t... Indices1, std::size_t... Indices2>
+constexpr ReturnType tuple_cat(Tuples&& ts,
+                               const std::index_sequence<Indices1...>&,
+                               const std::index_sequence<Indices2...>&) {
+  return ReturnType(shiro::get<Indices1>(shiro::get<Indices2>(ts))...);
 }
 
 }  // namespace tuple
@@ -199,10 +172,10 @@ template <typename... Tuples>
 constexpr typename shiro::detail::tuple::tuple_cat_return<Tuples...>::type
 tuple_cat(Tuples&&... ts) {
   return shiro::detail::tuple::tuple_cat<
-      typename shiro::detail::tuple::tuple_cat_return<Tuples...>::type>(
-      shiro::forward_as_tuple(std::forward<Tuples>(ts)...),
-      typename shiro::detail::tuple::tuple_cat_indices<Tuples...>::type{},
-      typename shiro::detail::tuple::tuple_cat_indices2<Tuples...>::type{});
+      typename shiro::detail::tuple::tuple_cat_return<Tuples...>::type
+    >(shiro::forward_as_tuple(std::forward<Tuples>(ts)...),
+      typename shiro::detail::tuple::make_tuple_cat_index1<Tuples...>::type{},
+      typename shiro::detail::tuple::make_tuple_cat_index2<Tuples...>::type{});
 }
 
 }  // namespace shiro
